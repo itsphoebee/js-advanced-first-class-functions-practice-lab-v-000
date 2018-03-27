51abe66252f12e0260e0fc0b6e05d6362ea0cacd
Phoebe Lau <phoebelau7@gmail.com>
2018-03-27 01:04:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const logDriverNames = function(drivers) {
 };
 
 const logDriversByHometown = function(drivers, location){
-  drivers.forEach(function(driver){
-    if driver.hometown === location
+  drivers.forEach(function(driver) {
+    driver.hometown === location
       console.log(driver.name);
   });
 };
